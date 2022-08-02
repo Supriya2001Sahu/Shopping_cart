4b1b7a0f8e6c591d8a06980d7e00ed5f7085f52e
Supriya2001Sahu <supriyasahups2001@gmail.com>
2022-08-03 00:54:47 +0530

    First Commit

---
+++ Announcement.js
@@ -0,0 +1,22 @@
+import styled from "styled-components"
+
+const Container = styled.div`
+height: 30px;
+background-color: #008080;
+color:white; 
+display: flex;
+align-items: center;
+justify-content: center;
+font-size: 14px;
+font-weight: 500;
+`
+
+const Announcement = () => {
+  return (
+    <Container>
+    Super Deal! Free Shipping on orders over $50
+    </Container>
+  )
+}
+
+export default Announcement
---
+++ data.js
@@ -0,0 +1,124 @@
+export const sliderItems = [
+    {
+      id: 1,
+      img: "https://freepngimg.com/download/shopping/6-2-shopping-transparent.png",
+      title: "SUMMER SALE",
+      desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+      bg: "f5fafd",
+    },
+    {
+      id: 2,
+      img: "https://i.ibb.co/DG69bQ4/2.png",
+      title: "AUTUMN COLLECTION",
+      desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+      bg: "fcf1ed",
+    },
+    {
+      id: 3,
+      img: "https://i.ibb.co/cXFnLLV/3.png",
+      title: "LOUNGEWEAR LOVE",
+      desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+      bg: "fbf0f4",
+    },
+  ];
+
+  export const categories = [
+    {
+      id: 1,
+      img: "https://images.pexels.com/photos/5886041/pexels-photo-5886041.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+      title: "SHIRT STYLE!",
+    },
+    {
+      id: 2,
+      img: "https://images.pexels.com/photos/2983464/pexels-photo-2983464.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+      title: "LOUNGEWEAR LOVE",
+    },
+    {
+      id: 3,
+      img: "https://images.pexels.com/photos/5480696/pexels-photo-5480696.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+      title: "LIGHT JACKETS",
+    },
+  ];
+
+  export const popularProducts = [
+    {
+      id:1,
+      img:"https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
+    },
+    {
+      id:2,
+      img:"https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg",
+    },
+    {
+      id:3,
+      img:"https://fakestoreapi.com/img/71li-ujtlUL._AC_UX679_.jpg",
+    },
+    {
+      id:4,
+      img:"https://fakestoreapi.com/img/71YXzeOuslL._AC_UY879_.jpg",
+    },
+    {
+      id:5,
+      img:"https://fakestoreapi.com/img/71pWzhdJNwL._AC_UL640_QL65_ML3_.jpg",
+    },
+    {
+      id:6,
+      img:"https://fakestoreapi.com/img/61sbMiUnoGL._AC_UL640_QL65_ML3_.jpg",
+    },
+    {
+      id:7,
+      img:"https://fakestoreapi.com/img/71YAIFU48IL._AC_UL640_QL65_ML3_.jpg",
+    },
+    {
+      id:8,
+      img:"https://fakestoreapi.com/img/51UDEzMJVpL._AC_UL640_QL65_ML3_.jpg",
+    },
+    {
+      id:9,
+      img:"https://fakestoreapi.com/img/61IBBVJvSDL._AC_SY879_.jpg",
+    },
+    {
+      id:10,
+      img:"https://fakestoreapi.com/img/61U7T1koQqL._AC_SX679_.jpg",
+    },
+    {
+      id:11,
+      img:"https://fakestoreapi.com/img/71kWymZ+c+L._AC_SX679_.jpg",
+    },
+    {
+      id:12,
+      img:"https://fakestoreapi.com/img/61mtL65D4cL._AC_SX679_.jpg",
+    },
+    {
+      id:13,
+      img:"https://fakestoreapi.com/img/81QpkIctqPL._AC_SX679_.jpg",
+    },
+    {
+      id:14,
+      img:"https://fakestoreapi.com/img/81Zt42ioCgL._AC_SX679_.jpg",
+    },
+    {
+      id:15,
+      img:"https://fakestoreapi.com/img/51Y5NI-I5jL._AC_UX679_.jpg",
+    },
+    {
+      id:16,
+      img:"https://fakestoreapi.com/img/81XH0e8fefL._AC_UY879_.jpg",
+    },
+    {
+      id:17,
+      img:"https://fakestoreapi.com/img/71HblAHs5xL._AC_UY879_-2.jpg",
+    },
+    {
+      id:18,
+      img:"https://fakestoreapi.com/img/71z3kpMAYsL._AC_UY879_.jpg",
+    },
+    {
+      id:19,
+      img:"https://fakestoreapi.com/img/51eg55uWmdL._AC_UX679_.jpg",
+    },
+    {
+      id:20,
+      img:"https://fakestoreapi.com/img/61pHAEJ4NML._AC_UX679_.jpg",
+    },
+  ]
